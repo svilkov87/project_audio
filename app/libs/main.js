4b3a04509450fa0780bfd1fe7110aa30e942098d
svilkov87 <svilkov87@mail.ru>
2017-02-06 23:34:43 +0300

    audio 0.5

--- app/libs/main.js
+++ app/libs/main.js
@@ -5,33 +5,26 @@ $(document).ready(function(){
     $(".about-ul").slideToggle(100);
   });
 
-  //мышка на/не выпавшем блоке о нас 
-  // $("#about_us").mouseover(function(){
-  //   $(".about-ul").fadeIn(500);
-  // });
-
-  // $(document).mouseup(function (e){ // событие клика по веб-документу
-  //   var block_about = $('.about-ul');// тут указываем ID элемента
-  //   if(!block_about.is(e.target) 
-  //   && block_about.has(e.target).length === 0){// если клик был не по нашему блоку
-  //       block_about.fadeOut();
-  //   }
-  // });
-
-
-
 // выпадающее меню
   $("#justify_nav").click(function(){
     $(".menu").fadeToggle(500);
   });
 
 
-// выпадающий поиск
+  // выпадающий поиск
 
   $("#stuff_menu_search").click(function(e){
     e.preventDefault();
     $(".stuff_menu_search_field").slideToggle(100);
         // $(".stuff_menu_search_field").css("display" , "block");
+  });
+
+    //Плавный скролл до блока .div по клику на .scroll
+  //Документация: https://github.com/flesler/jquery.scrollTo
+  $("#fa-angle-down").click(function() {
+    $.scrollTo($("#main_about"), 800, {
+      offset: 0
+    });
   });
 
 
