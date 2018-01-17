6171cbcdb4d5a4ac3f92d832c8bef57654120bcd
sergey vilkov <svilkov49@gmail.ru>
2018-01-17 23:44:27 +0300

    Change font

--- include/head.php
+++ include/head.php
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="shortcut icon" href="app/img/fav_micro.ico"/>
-<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link rel="stylesheet" href="app/libs/font-awesome-4.2.0/css/font-awesome.min.css" />
 <link rel="stylesheet" href="app/css/bootstrap.min.css">
 <link rel="stylesheet" href="app/css/animate.css">
